3903087de6b813ef19ca57f7120cda4af2bf6b4f
aquinoalejandro <aquinoalejandro666@gmail.com>
2024-08-22 01:03:51 -0300

    feat: :sparkles: Checklists funcionales

--- client/src/routes/MainRoutesPublic.jsx
+++ client/src/routes/MainRoutesPublic.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 
 import { Libreta } from "../pages/publicPages/Libreta"
+import { ToDo } from "../pages/publicPages/ToDo"
 
 
 export const MainRoutesPublic = () => {
@@ -10,6 +11,7 @@ export const MainRoutesPublic = () => {
             <Routes>
 
                 <Route path="/libreta" element={<Libreta />} />
+                <Route path="/todo" element={<ToDo />} />
 
             </Routes>
         </>
